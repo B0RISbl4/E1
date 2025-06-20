04a23ca453bda35140a018cc224e62260fdaaf9e
B0RISbl4 <BORISKAT110E4@YANDEX.RU>
2025-06-20 04:21:26 +0300

    3 points

--- E1/E1.cpp
+++ E1/E1.cpp
@@ -32,7 +32,7 @@ int main()
 
 int main()
 {
-	const string correctPin = "000000";
+	const string correctPin = "012345";
 	string foundPin;
 	
 	for (int i = 0; i <= 999999; i++) {
@@ -47,3 +47,37 @@ int main()
 	return 0;
 }
 */
+
+
+//3 point
+string formatPin(int pin) {
+	string formattedPin = to_string(pin);
+	while (formattedPin.length() < 6) {
+		formattedPin = "0" + formattedPin;
+	}
+	return formattedPin;
+}
+
+vector<int> findPin(int from, int to, const string correctPin) {
+	vector<int> foundPins;
+	for (int i = from; i <= to; i++) {
+
+		string curPin = formatPin(i);
+		if (curPin == correctPin) {
+			foundPins.push_back(i);
+		}
+	}
+	return foundPins;
+}
+
+	int main()
+{
+	const string correctPin = "012345";
+	vector<int> foundPin = findPin(0, 999999, correctPin);
+	string formattedFoundPin = formatPin(foundPin[0]);
+
+	formattedFoundPin == correctPin ? void(cout << "PIN: " << formattedFoundPin) : void();
+
+	cout << "\n";
+	return 0;
+}
