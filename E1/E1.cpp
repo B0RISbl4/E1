4b90c790368287c0f5446e7c1865c6fa4dcb8df1
B0RISbl4 <BORISKAT110E4@YANDEX.RU>
2025-06-21 23:22:37 +0300

    optimization commit

--- E1/E1.cpp
+++ E1/E1.cpp
@@ -3,53 +3,6 @@
 #include <vector>
 using namespace std;  
 
-/*
-//1 point
-
-int main()
-{
-	const string correctPin = "012345";
-	int attempts = 3;
-	string inputPin;
-
-	while(attempts > 0) {
-		cout << "PIN: ";
-		cin >> inputPin;
-		if (inputPin == correctPin) {
-			cout << "OK\n";
-			return 0;
-		} else {
-			attempts--;
-			cout << "NE OK\n";
-		}
-	}
-	return 0;
-}
-*/
-
-/*
-//2 point
-
-int main()
-{
-	const string correctPin = "012345";
-	string foundPin;
-	
-	for (int i = 0; i <= 999999; i++) {
-		foundPin = to_string(i);
-		while (foundPin.length() < 6) {
-			foundPin = "0" + foundPin;
-		}
-		foundPin == correctPin ? void(cout << "PIN: " << foundPin) : void();
-	}
-
-	cout << "\n";
-	return 0;
-}
-*/
-
-
-//3 point
 string formatPin(int pin) {
 	string formattedPin = to_string(pin);
 	while (formattedPin.length() < 6) {
