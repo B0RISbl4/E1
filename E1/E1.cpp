74d154b3c594ae80f5b51aa68e5b9d3448b0ba92
B0RISbl4 <BORISKAT110E4@YANDEX.RU>
2025-06-21 23:34:35 +0300

    final commit

--- E1/E1.cpp
+++ E1/E1.cpp
@@ -1,36 +1,23 @@
-﻿#include <iostream>  
-#include <string>
+﻿#include <iostream>
 #include <vector>
-using namespace std;  
+using namespace std;
 
-string formatPin(int pin) {
-	string formattedPin = to_string(pin);
-	while (formattedPin.length() < 6) {
-		formattedPin = "0" + formattedPin;
-	}
-	return formattedPin;
+vector<int> findPin(int from, int to, int correctPin) {
+    vector<int> foundPins;
+    for (int i = from; i <= to; i++) {
+        if (i == correctPin) {
+            foundPins.push_back(i);
+        }
+    }
+    return foundPins;
 }
 
-vector<int> findPin(int from, int to, const string correctPin) {
-	vector<int> foundPins;
-	for (int i = from; i <= to; i++) {
+int main() {
+    const int correctPin = 12345;
+    vector<int> foundPin = findPin(0, 999999, correctPin);
 
-		string curPin = formatPin(i);
-		if (curPin == correctPin) {
-			foundPins.push_back(i);
-		}
-	}
-	return foundPins;
-}
+    cout << "PIN: " << foundPin[0];
 
-	int main()
-{
-	const string correctPin = "012345";
-	vector<int> foundPin = findPin(0, 999999, correctPin);
-	string formattedFoundPin = formatPin(foundPin[0]);
-
-	formattedFoundPin == correctPin ? void(cout << "PIN: " << formattedFoundPin) : void();
-
-	cout << "\n";
-	return 0;
+    cout << "\n";
+    return 0;
 }
